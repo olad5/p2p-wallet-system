0aaf6103f339c288f079d7a19266a33e13c5f244
Francis Shonubi <shonubioladipo@gmail.com>
2022-07-31 16:52:55 +0100

    feat: add common routes base class for all routes
    - This class would be subclassed so all routes can conform to the same
      pattern

--- src/app.ts
+++ src/app.ts
@@ -11,10 +11,14 @@ import * as http from "http";
 import * as winston from "winston";
 import * as expressWinston from "express-winston";
 import cors from "cors";
+import debug from "debug";
+import { CommonRoutesConfig } from "./common/common.routes.config";
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
+const routes: CommonRoutesConfig[] = [];
 const port = process.env.PORT || 5200;
+const debugLog = debug("app");
 
 app.use(bodyParser.json({ limit: "31mb" }));
 app.use(bodyParser.urlencoded({ limit: "31mb", extended: true }));
@@ -40,5 +44,8 @@ app.get("/", (req: express.Request, res: express.Response) => {
 });
 
 export default server.listen(port, () => {
+  routes.forEach((route: CommonRoutesConfig) => {
+    debugLog(`Routes configured for ${route.getName()}`);
+  });
   console.log(runningMessage);
 });
