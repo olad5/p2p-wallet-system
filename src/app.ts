33b12cbfb20f528216a91be02c689d017681517c
Francis Shonubi <shonubioladipo@gmail.com>
2022-07-31 15:24:49 +0100

    feat: add http server and npm scripts

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import dotenv from "dotenv";
+
+const dotenvResult = dotenv.config();
+if (dotenvResult.error) {
+  throw dotenvResult.error;
+}
+
+import express from "express";
+import bodyParser from "body-parser";
+import * as http from "http";
+import * as winston from "winston";
+import * as expressWinston from "express-winston";
+import cors from "cors";
+
+const app: express.Application = express();
+const server: http.Server = http.createServer(app);
+const port = process.env.PORT || 5200;
+
+app.use(bodyParser.json({ limit: "31mb" }));
+app.use(bodyParser.urlencoded({ limit: "31mb", extended: true }));
+app.use(cors());
+
+const loggerOptions: expressWinston.LoggerOptions = {
+  transports: [new winston.transports.Console()],
+  format: winston.format.combine(
+    winston.format.json(),
+    winston.format.prettyPrint(),
+    winston.format.colorize({ all: true })
+  ),
+};
+if (!process.env.DEBUG) {
+  loggerOptions.meta = false;
+}
+app.use(expressWinston.logger(loggerOptions));
+
+const runningMessage = `Server running at http://localhoost:${port}`;
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.status(200).send(runningMessage);
+});
+
+export default server.listen(port, () => {
+  console.log(runningMessage);
+});
