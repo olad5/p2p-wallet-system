af6931c8b0915e548029c5542bd1092ecb098d11
Francis Shonubi <shonubioladipo@gmail.com>
2022-08-01 11:48:59 +0100

    feat: add common crud interface for model services
    - This interface would be implemented by various model services for crud
      operations

---
+++ src/common/interfaces/crud.interface.ts
@@ -0,0 +1,7 @@
+export interface CRUD<M, C, U> {
+  list: (limit: number) => Promise<M[]>;
+  create: (resource: C) => Promise<string>;
+  readById: (id: string) => Promise<M | null>;
+  patchById: (id: string, resource: U) => Promise<M | null>;
+  deleteById: (id: string) => Promise<M | null>;
+}
