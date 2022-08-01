7abd917f9b0ff789e9c11d9c2cdf6ed9eeade186
Francis Shonubi <shonubioladipo@gmail.com>
2022-08-01 11:51:01 +0100

    feat: add User service

---
+++ src/users/services/users.service.ts
@@ -0,0 +1,39 @@
+import { CRUD } from "../../common/interfaces/crud.interface";
+import { UserModel } from "../types/User.Model";
+import { CreateUserDto } from "../dtos/create.user.dto";
+import { PatchUserDto } from "../dtos/patch.user.dto";
+import UsersDao from "../daos/users.dao";
+
+class UserService implements CRUD<UserModel, CreateUserDto, PatchUserDto> {
+  async list(limit: number) {
+    const users = await UsersDao.getUsers(limit);
+    return users;
+  }
+
+  async create(resource: CreateUserDto) {
+    const newUser = await UsersDao.addUser(resource);
+    return newUser;
+  }
+
+  async readById(id: string) {
+    const user = await UsersDao.getUserById(id);
+    return user;
+  }
+
+  async patchById(id: string, resource: PatchUserDto) {
+    const updatedUser = await UsersDao.updateUserById(id, resource);
+    return updatedUser;
+  }
+
+  async deleteById(id: string) {
+    const deletedUser = await UsersDao.removeUserById(id);
+    return deletedUser;
+  }
+
+  async getUserByEmail(email: string) {
+    const user = await UsersDao.getUserByEmail(email);
+    return user;
+  }
+}
+
+export default new UserService();
