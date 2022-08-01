5a4c16c4aaafc7144917cf7e49527cec22f3cf54
Francis Shonubi <shonubioladipo@gmail.com>
2022-08-01 11:50:36 +0100

    feat: add User model interface

---
+++ src/users/types/User.Model.ts
@@ -0,0 +1,10 @@
+export interface UserModel {
+  id?: string;
+  email?: string;
+  password?: string;
+  firstName?: string;
+  lastName?: string;
+  wallet?: any;
+  created_at?: Date;
+  updated_at?: Date;
+}
