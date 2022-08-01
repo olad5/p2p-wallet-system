29176c0a04114f5569fb97853fc791f62e8699f6
Francis Shonubi <shonubioladipo@gmail.com>
2022-08-01 18:36:22 +0100

    feat: add auth middleware
    - this is to verify that a user has the correct email and password
      before they log in

---
+++ src/auth/middleware/auth.middleware.ts
@@ -0,0 +1,28 @@
+import { Response, Request, NextFunction } from "express";
+import * as argon2 from "argon2";
+import UsersService from "../../users/services/users.service";
+import debug from "debug";
+const log = debug("app:auth-middleware");
+
+class AuthMiddleware {
+  async verifyUserPassword(req: Request, res: Response, next: NextFunction) {
+    const user = await UsersService.getUserByEmailWithPassword(req.body.email);
+    log("confirmed user password with email", user);
+    if (user) {
+      const passwordHash = user.password;
+      if (await argon2.verify(passwordHash, req.body.password)) {
+        req.body = {
+          userId: user.id,
+          email: user.email,
+          permissionFlags: user.permissionFlags,
+        };
+        return next();
+      }
+    }
+    return res
+      .status(400)
+      .send({ status: "failed", errors: ["Invalid email and/or password"] });
+  }
+}
+
+export default new AuthMiddleware();
