52b6260993864c47963d8d59707a0c3e94a594d8
Francis Shonubi <shonubioladipo@gmail.com>
2022-08-01 10:32:57 +0100

    feat: add use daos for connecting to the Database

---
+++ src/users/daos/users.dao.ts
@@ -0,0 +1,86 @@
+import { CreateUserDto } from "../dtos/create.user.dto";
+import { PatchUserDto } from "../dtos/patch.user.dto";
+import PrismaService from "../../common/services/prisma.service";
+import debug from "debug";
+
+const log = debug("app:UsersDaos");
+
+class UsersDaos {
+  private prismaClient = this.prismaService.getPrismaClient();
+
+  constructor(private prismaService: typeof PrismaService) {
+    log("Creatde new Instance of UserDaos");
+  }
+
+  async addUser(userFields: CreateUserDto) {
+    const newUser = await this.prismaClient.user.create({
+      data: {
+        email: userFields.email,
+        password: userFields.password,
+        firstName: userFields.firstName,
+        lastName: userFields.lastName,
+      },
+    });
+    await this.prismaClient.wallet.create({
+      data: {
+        user_id: newUser.id,
+      },
+    });
+    return newUser.id;
+  }
+
+  async getUserById(userId: string) {
+    const user = await this.prismaClient.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    return user;
+  }
+
+  async getUsers(limit: number) {
+    const users = await this.prismaClient.user.findMany({
+      take: limit,
+    });
+
+    return users;
+  }
+
+  async getUserByEmail(userEmail: string) {
+    const user = await this.prismaClient.user.findUnique({
+      where: {
+        email: userEmail,
+      },
+    });
+    return user;
+  }
+
+  async updateUserById(userId: string, userFields: PatchUserDto) {
+    const existingUser = await this.prismaClient.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        ...userFields,
+      },
+    });
+    return existingUser;
+  }
+
+  async removeUserById(userId: string) {
+    const deletedUser = await this.prismaClient.user.delete({
+      where: {
+        id: userId,
+      },
+      select: {
+        email: true,
+        firstName: true,
+        lastName: true,
+      },
+    });
+    return deletedUser;
+  }
+}
+
+export default new UsersDaos(PrismaService);
